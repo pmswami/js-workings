7c91744b85fe187e86a0da43de861b3ee0133b1a
swamfire <usern5949@gmail.com>
2023-09-23 21:37:04 +0530

    added scoping

--- script.js
+++ script.js
@@ -5,3 +5,33 @@
 //     3. Block scope
 
 // only let and const are block scope
+
+function calcAge(birthYear){
+    const age = 2037 - birthYear
+    // console.log("First Name from function",firstName)
+    // console.log(lastName)
+
+    function printAge(){
+        // Creating new varibale with same name as outer scope variable
+        const firstName = "EchoEcho"
+        const output = `${firstName}, you are ${age} years old, born in ${birthYear}`
+        console.log(output)
+        if(birthYear>=1981 && birthYear<=1996){
+            var millenial = true
+            const str = `${firstName} you are millenial`
+            console.log(str)
+            function add(a, b){
+                return a+ b
+            }
+        }
+        // console.log(millenial)
+        // console.log(add(3, 4))
+    }
+    // console.log(millenial)
+    printAge()
+    return age
+}
+
+const firstName = "SwamFire"
+calcAge(1990)
+// console.log("first name", firstName)

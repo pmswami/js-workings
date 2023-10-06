9439bf0b4690ee4c2eb8500311415503404b467b
swamfire <usern5949@gmail.com>
2023-10-06 21:58:53 +0530

    added immediately invoked function expression

--- js-functions/script.js
+++ js-functions/script.js
@@ -1,29 +1,41 @@
 'use strict';
 
-const poll = {
-    question: 'What is your favourite programming language?',
-    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-    // This generates [0, 0, 0, 0]. More in the next section 😃
-    answers: new Array(4).fill(0),
-    registerNewAnswer() {
-        const answer = Number(prompt(`${this.question}\n${this.options.join("\n")}`));
-        // console.log(answer);
-        typeof answer == "number" && answer < this.answers.length && this.answers[answer]++;
-        // console.log(this.answers);
-        this.displayResults();
-        this.displayResults("string");
-    },
-    displayResults(type = "array") {
-        if (type === "array") {
-            console.log(this.answers);
-        }
-        else if (type == "string") {
-            console.log(`Poll results are ${this.answers.join(", ")}`);
-        }
-    },
-};
-// console.log(poll.registerNewAnswer());
-document.querySelector(".poll").addEventListener("click", poll.registerNewAnswer.bind(poll));
+//_____________ Immediately invoked funcion expression ___________________
+(function () {
+    console.log("This will never run again ");
+})();
+(() => {
+    console.log("This will also never run again ");
+})();
+
+// // ______________________ Challenge #1 ________________________________
+// const poll = {
+//     question: 'What is your favourite programming language?',
+//     options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+//     // This generates [0, 0, 0, 0]. More in the next section 😃
+//     answers: new Array(4).fill(0),
+//     registerNewAnswer() {
+//         const answer = Number(prompt(`${this.question}\n${this.options.join("\n")}`));
+//         // console.log(answer);
+//         typeof answer == "number" && answer < this.answers.length && this.answers[answer]++;
+//         // console.log(this.answers);
+//         this.displayResults();
+//         this.displayResults("string");
+//     },
+//     displayResults(type = "array") {
+//         if (type === "array") {
+//             console.log(this.answers);
+//         }
+//         else if (type == "string") {
+//             console.log(`Poll results are ${this.answers.join(", ")}`);
+//         }
+//     },
+// };
+// // console.log(poll.registerNewAnswer());
+// document.querySelector(".poll").addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+
+
 // //_____________________ CALL and APPLY Methods __________________________________
 
 // const lufthansa = {

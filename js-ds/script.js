34a101dd22fd0c7f34dc30e1415b70f2c5a156fc
swamfire <usern5949@gmail.com>
2023-10-06 12:12:08 +0530

    Added call and apply methods

--- js-ds/script.js
+++ js-ds/script.js
@@ -81,25 +81,58 @@ const restaurant = {
   },
 };
 
-//_______________ Function returning other function ________________________
-// JS closures === Python decorators
-// const greet = function (greeting) {
-//   return function (name) {
-//     console.log(`${greeting} ${name}`);
-//   };
-// };
+//_____________________ CALL and APPLY Methods __________________________________
 
-// const greet = (greeting) => {
-//   return (name) => {
-//     console.log(`${greeting} ${name}`);
-//   };
-// };
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  book: function (flightNum, name) {
+    console.log(`${name} has booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  }
+};
+lufthansa.book(123, "SwamFire");
+lufthansa.book(456, "XLR8");
+console.log(lufthansa);
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+const book = lufthansa.book;
+book.call(eurowings, 123, "EchoEcho");
+console.log(eurowings);
+book.call(lufthansa, 123, "EchoEcho");
+console.log(lufthansa);
 
-const greet = greeting => name => console.log(`${greeting} ${name}`);
+const flightData = [789, "HeatBlast"];
+book.apply(eurowings, flightData);
+console.log(eurowings);
+book.call(lufthansa, ...flightData);
+console.log(lufthansa);
 
-const greeterHey = greet("Hey");
-greeterHey("SwamFire");
-greet("Hello")("XLR8");
+// //_______________ Function returning other function ________________________
+// // JS closures === Python decorators
+// // const greet = function (greeting) {
+// //   return function (name) {
+// //     console.log(`${greeting} ${name}`);
+// //   };
+// // };
+
+// // const greet = (greeting) => {
+// //   return (name) => {
+// //     console.log(`${greeting} ${name}`);
+// //   };
+// // };
+
+// const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+// const greeterHey = greet("Hey");
+// greeterHey("SwamFire");
+// greet("Hello")("XLR8");
+
+
 // //____________________ Higher Order Functions __________________________________
 // const oneWord = function (str) {
 //   return str.replace(/ /g, "").toLowerCase();

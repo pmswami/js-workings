134cd1ff3e4fa5c6033c8db19e933c9a5a71d7bf
swamfire <usern5949@gmail.com>
2023-10-07 22:54:50 +0530

    challenge #2

--- js-arrays/script.js
+++ js-arrays/script.js
@@ -175,3 +175,20 @@ displayMovements(account1.movements);
 //   console.log(`Set:${set}\t${key}:${value}`);
 // });
 
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  console.log(dogsJuliaCorrected);
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  console.log(dogs);
+  dogs.forEach(function (dog, i) {
+    if (dog > 3) {
+      console.log("Dog is adult");
+    }
+    else {
+      console.log("Still a puppy");
+    }
+  });
+};
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);

3826a8b609b99e483ef6fd0c99242f15716ec944
swamfire <usern5949@gmail.com>
2023-10-31 17:13:18 +0530

    Added dates and timestamps

--- js-numbersAndDates/script.js
+++ js-numbersAndDates/script.js
@@ -357,36 +357,67 @@ btnSort.addEventListener('click', function (e) {
 // // numeric separators are only supported with numbers not with strings
 
 
-// _______________ BigInt _______________________________
-console.log("BigInt\n");
-console.log("MAX_INT", 2 ** 53 - 1);
-console.log(Number.MAX_SAFE_INTEGER);
-console.log(2 ** 53 + 1);
-console.log(2 ** 53 + 2);
-console.log(2 ** 53 + 3);
-console.log(2 ** 53 + 4);
-console.log(2 ** 53 + 5);
-console.log(87846298485291467685843281678n);
-const bigInt = BigInt(8784629878);
-console.log(bigInt);
+// // _______________ BigInt _______________________________
+// console.log("BigInt\n");
+// console.log("MAX_INT", 2 ** 53 - 1);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(2 ** 53 + 1);
+// console.log(2 ** 53 + 2);
+// console.log(2 ** 53 + 3);
+// console.log(2 ** 53 + 4);
+// console.log(2 ** 53 + 5);
+// console.log(87846298485291467685843281678n);
+// const bigInt = BigInt(8784629878);
+// console.log(bigInt);
 
-// BigInt operations
-console.log(10000n + 20000n);
-console.log(545754545414524545154n * 20000n);
-// console.log(8548264783691829367816387n + 23); //error
-console.log(8548264783691829367816387n + BigInt(23));
+// // BigInt operations
+// console.log(10000n + 20000n);
+// console.log(545754545414524545154n * 20000n);
+// // console.log(8548264783691829367816387n + 23); //error
+// console.log(8548264783691829367816387n + BigInt(23));
 
-// Exceptions
-console.log(20n > 15);
-console.log(20 === 20); // true
-console.log(20 === 20n); // false
-console.log(typeof (20n));
-console.log(20n == 20); // true
+// // Exceptions
+// console.log(20n > 15);
+// console.log(20 === 20); // true
+// console.log(20 === 20n); // false
+// console.log(typeof (20n));
+// console.log(20n == 20); // true
 
-// Math operaions like sqrt, does not work with bigint
-// console.log(Math.sqrt(16n)); // error
-console.log(207867579578567547589589554747454n + " is really a big integer");
+// // Math operaions like sqrt, does not work with bigint
+// // console.log(Math.sqrt(16n)); // error
+// console.log(207867579578567547589589554747454n + " is really a big integer");
 
-// Divisions
-console.log(10n / 3n); // cuts decimal part
-console.log(10 / 3);
+// // Divisions
+// console.log(10n / 3n); // cuts decimal part
+// console.log(10 / 3);
+
+// _________________________ DATES and TIME _________________________________
+// Date creation
+let now = new Date();
+console.log(now);
+// convert string to date
+console.log(new Date("Oct 31 2023 16:49:03"));
+console.log(new Date("December 24, 2015"));
+console.log(account1.movementsDates[0]);
+console.log("converted into date", new Date(account1.movementsDates[0]));
+console.log(new Date(1995, 10, 27, 15, 15, 15));
+console.log(new Date(2020, 10, 31)); // auto corrected date( there is no 31st nov u JS converts it to next date)
+console.log(new Date(0)); // Considered as per unix start time
+console.log(new Date(3 * 24 * 60 * 60 * 1000)); //uses millisecond value
+
+//working with dates
+console.log("Working with dates\n");
+const future = new Date(1995, 10, 27, 15, 15, 1);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getDate());
+console.log(future.getDay()); // returns day of the week
+console.log(future.getMonth());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime()); //returns time in milliseconds
+console.log(Date.now()); // returns current timestamp in milliseconds
+future.setFullYear(2020);
+console.log(future);

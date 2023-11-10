c23d6783d27ab4d414769ac8dbb22e58c8222b6e
swamfire <usern5949@gmail.com>
2023-11-10 19:41:59 +0530

    Added create insert and remove elements

--- js-advancedDOM/script.js
+++ js-advancedDOM/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (event) {
+  event.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(button => button.addEventListener('click', openModal));
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +32,39 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+// _________________ SELECTING ELEMENTS _______________________________
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector(".header");
+console.log(header);
+const allSections = document.querySelectorAll(".section");
+console.log(allSections);
+
+const elementById = document.getElementById("section--1");
+console.log(elementById);
+
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+
+const classBtn = document.getElementsByClassName("btn");
+console.log(classBtn);
+
+// _______________ Creating and inserting elements ________________________
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.textContent = "We use cookies for improved functionality and analytics.";
+message.innerHTML = "We use cookies for improved functionality and analytics.<button class='btn btn--close-cookie'>Got It!</button>";
+// header.prepend(message);
+// header.append(message);
+// header.append(message.cloneNode(true));
+header.before(message); // before the header element
+// header.after(message); // after header element
+
+
+// Delete Element
+document.querySelector(".btn--close-cookie").addEventListener("click", () => (message.remove()));

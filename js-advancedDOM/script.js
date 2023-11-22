67f8d246f10beca78e229b68030d55a2f2c5ab93
swamfire <usern5949@gmail.com>
2023-11-22 22:19:49 +0530

    added implimenting sticky navigation bar

--- js-advancedDOM/script.js
+++ js-advancedDOM/script.js
@@ -351,3 +351,19 @@ const handleHover = function (e) {
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 // nav.addEventListener("mouseout", function (e) { handleHover(e, 1); });
 nav.addEventListener("mouseout", handleHover.bind(1));
+
+
+
+// ___________ Implementing sticky navigation bar ___________
+
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+window.addEventListener("scroll", function (e) {
+  // console.log(e);
+  // console.log(window.scrollY);
+  if (this.window.scrollY > initialCoords.top) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+});

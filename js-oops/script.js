2fa6bcce343f08e54d0f24ccd6d4ecd5268565e5
swamfire <usern5949@gmail.com>
2023-12-10 16:28:55 +0530

    added OOP concepts in JS

--- js-oops/script.js
+++ js-oops/script.js
@@ -1,1 +1,24 @@
 'use strict';
+// OOP is programming paradigm to write and oranize code
+// all related data and method are into one class
+// Objects contains data and methods
+// Objects are created from classes
+// Classes are blueprints for creating objects
+// Classes are used to define properties and methods
+// Classes are used to create objects
+// Classes are used to define the structure of objects
+// Classes are used to define the behavior of objects
+// OOP is used to remove spaghetti code
+// OOP is used to make code more modular
+// OOP is used to make code more reusable
+// OOP is used to make code more maintainable
+// OOP is used to make code more readable
+// OOP is used to make code more efficient
+// OOP is used to make code more scalable
+// OOP is used to make code more secure
+
+// Fundamental Principles of OOP
+// 1. Abstraction -> Hiding the details which dont matter to the user
+// 2. Encapsulation -> Keeping some properties private inside the class, which cant be accessed from outside of the class
+// 3. Inheritance -> One class can inherit the properties of other class.
+// 4. Polymorphism -> A child can overwrite a method inherited from parent class
